f9bfb23f92aae2a721b908ade5dffc8eb9924669
cyrilf <cyrilf7x@gmail.com>
2020-04-01 00:35:02 +0200

    Fix minor accessibility issues

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -17,6 +17,7 @@
           <button
             @click="toggle"
             class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600"
+            aria-label="Menu toggle"
           >
             <svg
               class="current-color h-3 w-3"
